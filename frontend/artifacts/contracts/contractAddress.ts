1e69be4596ab1274d0978122a524f069c5709464
Armando F <flores@mtu.edu>
2022-10-08 17:16:28 -0400

    idk

--- frontend/artifacts/contracts/contractAddress.ts
+++ frontend/artifacts/contracts/contractAddress.ts
@@ -1,1 +1,1 @@
-export const Market = '0x8049ad567CB4265a42213ec0ef939Cdb13E66595'
+export const Market = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
--- frontend/pages/nft/[chain]/[contract]/[tokenId].tsx
+++ frontend/pages/nft/[chain]/[contract]/[tokenId].tsx
@@ -1,8 +1,13 @@
 import {
   Box,
   Button,
+  useColorMode,
+  Flex,
+  Grid,
+  GridItem,
   Heading,
   Img,
+  Image,
   Text,
   Modal,
   ModalBody,
@@ -11,10 +16,10 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Spacer,
   Spinner,
   useDisclosure,
   Link,
-  Flex,
 } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
@@ -63,6 +68,8 @@ const NftIndex: NextPage = () => {
   const [transactionState, setTransactionState] = useState<TransactionState>(
     TransactionState.notStarted
   )
+  const { colorMode, toggleColorMode } = useColorMode()
+
   const { isOpen, onOpen, onClose } = useDisclosure()
   const listedWithUs =
     nftOwner != null &&
@@ -188,27 +195,30 @@ const NftIndex: NextPage = () => {
     }
 
     return (
-      <>
-        <Heading marginBottom={5}>{nft?.title}</Heading>
-        <Text fontSize="lg" marginBottom={2}>
-          {nft?.description}
-        </Text>
-        <Text fontSize="md">
-          {listedWithUs ? 'Listed on name placholder' : `Owned by ${nftOwner}`}
-        </Text>
+      <Box>
         <Img
           src={image}
           alt={nft?.title}
-          boxSize="sm"
-          marginBottom={10}
-          marginTop={10}
+          boxSize="lg"
+          borderRadius="xl"
+          mx="auto"
+          marginBottom={5}
+          objectFit="cover"
         />
-        <Button onClick={onOpen}>
-          {userConnectedChain
-            ? `Buy NFT on ${userConnectedChain.name}`
-            : `Connect wallet to buy NFT`}
-        </Button>
-      </>
+        <div>
+          <Flex flexDirection="column">
+            <Text fontSize="xs">Contract Address:</Text>
+            <Text fontSize="xl">{`${contract}`}</Text>
+          </Flex>
+        </div>
+
+        <div>
+          <Flex flexDirection="column">
+            <Text fontSize="xs">TokenId:</Text>
+            <Text fontSize="xl">{`${tokenId}`}</Text>
+          </Flex>
+        </div>
+      </Box>
     )
   }
 
@@ -220,7 +230,10 @@ const NftIndex: NextPage = () => {
       return <Spinner />
     }
 
-    if (listingInfo == null || isErrorListingInfo) {
+    console.log(listingInfo)
+    console.log(isErrorListingInfo)
+
+    if (listingInfo == null) {
       return (
         <Text fontSize="lg">
           Something went wrong while fetching listing Info
@@ -229,36 +242,120 @@ const NftIndex: NextPage = () => {
     }
 
     return (
-      <>
-        <Text fontSize="md">{`seller: ${listingInfo[0]}`}</Text>
-        <Text fontSize="md">{`price: ${formatEther(listingInfo[1])}`}</Text>
-        <Text fontSize="md">{`currencyId: ${listingInfo[2]}`}</Text>
-      </>
+      <Box>
+        <Heading marginBottom={5}>{nft?.title}</Heading>
+        <Text fontSize="lg">{nft?.description}</Text>
+        <Box marginTop={10}>
+          <div>
+            <Flex>
+              <Image
+                mb={4}
+                ml={5}
+                pr={5}
+                src="/owner-icon.svg"
+                fallbackSrc="/owner-icon.svg"
+                borderRadius="full"
+              />
+              <Flex flexDirection="column">
+                <Text fontSize="xs">Owner:</Text>
+                <Text
+                  fontSize="xl"
+                  as="b"
+                  maxWidth="350px"
+                  noOfLines={1}
+                  textTransform="uppercase"
+                >{listingInfo[0]}</Text>
+              </Flex>
+            </Flex>
+          </div>
+          <div>
+            <Flex>
+              <Image
+                mb={4}
+                ml={5}
+                pr={5}
+                src="/chain-icon.svg"
+                fallbackSrc="/chain-icon.svg"
+              />
+              <Flex flexDirection="column">
+                <Text fontSize="xs">Chain:</Text>
+                <Text fontSize="xl" as="b">{`${listingChain}`}</Text>
+              </Flex>
+            </Flex>
+          </div>
+          <div>
+            <Flex>
+              <Image
+                mb={4}
+                ml={5}
+                pr={5}
+                src="/price-icon.svg"
+                fallbackSrc="/price-icon.svg"
+                borderRadius="full"
+              />
+              <Flex flexDirection="column">
+                <Text fontSize="xs">Price:</Text>
+                <Text fontSize="xl" as="b">
+                {formatEther(listingInfo[1])}
+                </Text>
+              </Flex>
+            </Flex>
+          </div>
+        </Box>
+        <Box>
+        <Button onClick={onOpen}>
+          {userConnectedChain
+            ? `Buy NFT on ${userConnectedChain.name}`
+            : `Connect wallet to buy NFT`}
+        </Button>        
+        </Box>
+      </Box>
     )
   }
 
   return (
     <Layout>
-      <Box>
-        {nftData(nft, isLoadingMetadata)}
-        {listingData(listingInfo, isLoadingListingInfo)}
-        <Box marginTop={10}>
-          <div>{`current wallet address: ${address}`}</div>
-          <div>{`listing chain: ${listingChain}`}</div>
-          <div>{`contract address: ${contract}`}</div>
-          <div>{`tokenId: ${tokenId}`}</div>
-          <div>{`NFT seller currenctly connected: ${sellerConnected}`}</div>
-        </Box>
-      </Box>
-      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Transaction Summary</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>Insightful Transaction Summary</ModalBody>
-          {modalInterior(transactionState)};
-        </ModalContent>
-      </Modal>
+      <Button onClick={toggleColorMode} size={'sm'} variant="link">
+        {colorMode === 'light' ? <Text> </Text> : <Text> </Text>}
+      </Button>
+
+      <Grid
+        h="500px"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(2, 1fr)"
+        gap={4}
+      >
+        <GridItem
+          colSpan={1}
+          bg="#170D27"
+          borderRadius="xl"
+          borderColor="#ffffff"
+          border="5px"
+          p="10"
+        >
+          {nftData(nft, isLoadingMetadata)}
+        </GridItem>
+
+        <GridItem
+          colSpan={1}
+          bg="#170D27"
+          borderRadius="xl"
+          borderColor="#ffffff"
+          border="5px"
+          p="10"
+        >
+          {listingData(listingInfo, isLoadingListingInfo)}
+        </GridItem>
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Transaction Summary</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>Insightful Transaction Summary</ModalBody>
+            {modalInterior(transactionState)};
+          </ModalContent>
+        </Modal>
+      </Grid>
     </Layout>
   )
 }
