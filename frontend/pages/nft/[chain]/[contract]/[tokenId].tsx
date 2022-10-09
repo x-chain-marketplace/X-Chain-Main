5dcd4ae87ac246645687536e861551e167dd5e1e
Armando F <flores@mtu.edu>
2022-10-08 22:22:31 -0400

    extra styling

--- frontend/pages/nft/[chain]/[contract]/[tokenId].tsx
+++ frontend/pages/nft/[chain]/[contract]/[tokenId].tsx
@@ -187,11 +187,34 @@ const NftIndex: NextPage = () => {
     switch (transactionState) {
       case TransactionState.notStarted:
         return (
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={buyNFT}>
+          <ModalFooter display="flex" justifyContent="center">
+            <Button
+              background="#FF6600"
+              mr={3}
+              border="2px solid #FF6600"
+              padding="25px 40px"
+              color="#fff"
+              borderRadius="120px"
+              fontSize="20px"
+              onClick={buyNFT}
+              _hover={{
+                background: '#ff660099',
+                transition: '0.5s',
+                transform: 'scale(1.05)',
+                color: '#fff',
+              }}
+            >
               Buy NFT
             </Button>
-            <Button onClick={onClose}>Back</Button>
+
+            <Button
+              border="2px solid #ccc"
+              borderRadius="120px"
+              padding="25px 30px"
+              onClick={onClose}
+            >
+              Back
+            </Button>
           </ModalFooter>
         )
       case TransactionState.pending:
@@ -219,7 +242,9 @@ const NftIndex: NextPage = () => {
             marginBottom={10}
             marginTop={10}
           >
-            <Text color="ffffff" marginBottom={5}>Yay everything worked</Text>
+            <Text color="ffffff" marginBottom={5}>
+              Yay everything worked
+            </Text>
             <Link
               fontSize="md"
               href="https://etherscan.io/tx/0x077b71c0517104c8a47fd6eb3415c436a472bfe77322dcd6c0de561b358317f9"
@@ -259,15 +284,19 @@ const NftIndex: NextPage = () => {
         />
         <div>
           <Flex flexDirection="column">
-            <Text color="#ffffff" fontSize="xs">Contract Address:</Text>
-            <Text color="#ffffff" fontSize="xl">{`${contract}`}</Text>
+            <Text color="#ffffff" fontSize="xs">
+              Contract Address:
+            </Text>
+            <Text color="#ffffff" fontSize="xl" as="b">{`${contract}`}</Text>
           </Flex>
         </div>
 
         <div>
           <Flex flexDirection="column">
-            <Text color="#ffffff" mt="3" fontSize="xs">TokenId:</Text>
-            <Text color="#ffffff" fontSize="xl">{`${tokenId}`}</Text>
+            <Text color="#ffffff" mt="3" fontSize="xs">
+              TokenId:
+            </Text>
+            <Text color="#ffffff" fontSize="xl" as="b">{`${tokenId}`}</Text>
           </Flex>
         </div>
       </Box>
@@ -296,8 +325,12 @@ const NftIndex: NextPage = () => {
           <Spinner />
         ) : (
           <>
-            <Heading color="#ffffff" marginBottom={5}>{nft?.title}</Heading>
-            <Text color="#ffffff" fontSize="lg">{nft?.description}</Text>
+            <Heading color="#ffffff" marginBottom={5}>
+              {nft?.title}
+            </Heading>
+            <Text color="#ffffff" fontSize="lg">
+              {nft?.description}
+            </Text>
             <Box marginTop={10}>
               <div>
                 <Flex>
@@ -310,7 +343,9 @@ const NftIndex: NextPage = () => {
                     borderRadius="full"
                   />
                   <Flex flexDirection="column">
-                    <Text color="#ffffff" fontSize="xs">Owner:</Text>
+                    <Text color="#ffffff" fontSize="xs">
+                      Owner:
+                    </Text>
                     <Text
                       fontSize="xl"
                       as="b"
@@ -332,7 +367,9 @@ const NftIndex: NextPage = () => {
                     fallbackSrc="/chain-icon.svg"
                   />
                   <Flex flexDirection="column">
-                    <Text color="#ffffff" fontSize="xs">Chain:</Text>
+                    <Text color="#ffffff" fontSize="xs">
+                      Chain:
+                    </Text>
                     <Text fontSize="xl" as="b" color="#ffffff">
                       {listingChain}
                     </Text>
@@ -350,7 +387,9 @@ const NftIndex: NextPage = () => {
                     borderRadius="full"
                   />
                   <Flex flexDirection="column">
-                    <Text color="#ffffff" fontSize="xs">Price:</Text>
+                    <Text color="#ffffff" fontSize="xs">
+                      Price:
+                    </Text>
                     <Text color="#ffffff" fontSize="xl" as="b">
                       {formatEther(listingInfo[1])}
                     </Text>
@@ -358,10 +397,27 @@ const NftIndex: NextPage = () => {
                 </Flex>
               </div>
             </Box>
-            <Box>
+            <Box
+              display="flex"
+              justifyContent="center"
+              padding="40px"
+              background="linear-gradient(180deg, #27183F 0%, #170D27 100%);"
+            >
               <Button
                 onClick={onOpen}
                 disabled={writeAsync == null || userConnectedChain == null}
+                background="#FF6600"
+                border="2px solid #FF6600"
+                padding="25px 20px"
+                color="#fff"
+                borderRadius="120px"
+                fontSize="20px"
+                _hover={{
+                  background: '#ff660099',
+                  transition: '0.5s',
+                  transform: 'scale(1.05)',
+                  color: '#fff',
+                }}
               >
                 {userConnectedChain
                   ? `Buy on ${userConnectedChain.name}`
@@ -417,7 +473,7 @@ const NftIndex: NextPage = () => {
       >
         <GridItem
           colSpan={1}
-          bg="#170D27"
+          bg="linear-gradient(180deg, #27183F 0%, #170D27 100%);"
           borderRadius="xl"
           borderColor="#ffffff"
           border="5px"
@@ -427,7 +483,7 @@ const NftIndex: NextPage = () => {
         </GridItem>
         <GridItem
           colSpan={1}
-          bg="#170D27"
+          bg="linear-gradient(180deg, #27183F 0%, #170D27 100%);"
           borderRadius="xl"
           borderColor="#ffffff"
           border="5px"
@@ -439,23 +495,123 @@ const NftIndex: NextPage = () => {
 
       <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Transaction Summary</ModalHeader>
+        <ModalContent h="700px" maxW="850px">
+          <ModalHeader mx="auto">
+            <Text fontSize="34px">Transaction Summary</Text>
+          </ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <Flex justifyContent="space-between">
-              <Stat>
-                <StatLabel>You're paying</StatLabel>
-                <StatNumber>{`${listingInfo && formatEther(listingInfo[1])} ${
-                  userConnectedChain?.nativeCurrency?.name
-                }`}</StatNumber>
-                <StatHelpText>{`on ${userConnectedChain?.name} chain`}</StatHelpText>
+              <Stat mr="5">
+                <StatLabel fontSize="lg" mb="2">
+                  You're Paying
+                </StatLabel>
+                <Box
+                  p="5"
+                  border="1px"
+                  display="flex"
+                  borderRadius="xl"
+                  flexDirection="column"
+                  justifyContent="center"
+                  background="linear-gradient(180deg, #27183F 0%, #170D27 100%)"
+                  minHeight="400px"
+                  borderColor="#ccc"
+                >
+                  <div>
+                    <Flex>
+                      <Image
+                        mb={10}
+                        ml={5}
+                        pr={5}
+                        src="/price-icon.svg"
+                        fallbackSrc="/price-icon.svg"
+                      />
+                      <Flex flexDirection="column">
+                        <Text color="#000" fontSize="xs" color="#fff">
+                          Price:
+                        </Text>
+                        <Text color="#000" fontSize="xl" as="b" color="#fff">
+                          <StatNumber>{`${
+                            listingInfo && formatEther(listingInfo[1])
+                          } ${
+                            userConnectedChain?.nativeCurrency?.name
+                          }`}</StatNumber>
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </div>
+                  <div>
+                    <Flex>
+                      <Image
+                        mb={4}
+                        ml={5}
+                        pr={5}
+                        src="/chain-icon.svg"
+                        fallbackSrc="/chain-icon.svg"
+                      />
+                      <Flex flexDirection="column">
+                        <Text color="#000" fontSize="xs" color="#fff">
+                          Chain:
+                        </Text>
+                        <Text color="#000" fontSize="xl" as="b" color="#fff">
+                          {userConnectedChain?.name}
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </div>
+                </Box>
               </Stat>
-              <Stat>
-                <StatLabel>You're recieving</StatLabel>
-                <StatNumber>{nft?.title}</StatNumber>
-                <Img boxSize="xs" src={image} />
-                <StatHelpText>{`on ${listingChain} chain`}</StatHelpText>
+              <Stat ml="5">
+                <StatLabel fontSize="lg" mb="2">
+                  You're Recieving
+                </StatLabel>
+                <Box
+                  p="5"
+                  border="1px"
+                  borderColor="#ccc"
+                  minHeight="400px"
+                  background="linear-gradient(180deg, #27183F 0%, #170D27 100%)"
+                  borderRadius="xl"
+                >
+                  <Img
+                    display="block"
+                    mx="auto"
+                    boxSize="xs"
+                    width="200px"
+                    height="100%"
+                    src={image}
+                  />
+                  <StatNumber
+                    display="flex"
+                    color="#fff"
+                    justifyContent="center"
+                    mt="2"
+                    fontSize="xl"
+                    mb="5"
+                  >
+                    {nft?.title}
+                  </StatNumber>
+
+                  <div>
+                    <Flex>
+                      <Image
+                        mb={4}
+                        ml={5}
+                        pr={5}
+                        src="/chain-icon.svg"
+                        fallbackSrc="/chain-icon.svg"
+                      />
+                      <Flex flexDirection="column">
+                        <Text color="#fff" fontSize="xs">
+                          Chain:
+                        </Text>
+                        <Text color="#fff" fontSize="xl" as="b">
+                          {listingChain}
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </div>
+                </Box>
               </Stat>
             </Flex>
           </ModalBody>
